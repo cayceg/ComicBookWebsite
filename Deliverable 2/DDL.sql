fa9ffb4afb8c77f59f853ff6eb95d56330f7796c
Andrew Gaal <gaal0007@algonquinlive.com>
2023-02-08 10:47:06 -0500

    Initial Commit

---
+++ Deliverable 2/DDL.sql
@@ -0,0 +1,26 @@
+drop database if exists capstone;
+create database capstone;
+
+drop table if exists capstone.customer;
+create table capstone.customer
+(
+	customerId			varchar(50)		not null,
+    displayName			varchar(50),
+    email				varchar(50),
+    pass				varchar(50),
+    registerDate		date,
+    postCount			int,
+    verificationCode    varchar(10),
+    CONSTRAINT PK_Customer PRIMARY KEY (customerId)
+);
+
+drop table if exists capstone.product;
+create table capstone.product
+(
+	productId			varchar(50) 	not null,
+    productName			varchar(50),
+    productDescription	varchar(50),
+    price				double,
+    clickCount			int,
+    CONSTRAINT PK_Product PRIMARY KEY (productID)
+);
---
+++ Deliverable 2/insert_products.sql
@@ -0,0 +1,14 @@
+insert into capstone.product (productId, productName, productDescription, price, clickCount)
+values("5828be48-419e-11ec-81d3-0242ac130003", "Superman", "A comic book about Superman", 10.00, 0);
+
+insert into capstone.product (productId, productName, productDescription, price, clickCount)
+values("5828c078-419e-11ec-81d3-0242ac130003", "Batman", "A comic book about Batman", 10.00, 0);
+
+insert into capstone.product (productId, productName, productDescription, price, clickCount)
+values("5828c2c6-419e-11ec-81d3-0242ac130003", "Spiderman", "A comic book about Spiderman", 10.00, 0);
+
+insert into capstone.product (productId, productName, productDescription, price, clickCount)
+values("5828c3ac-419e-11ec-81d3-0242ac130003", "Wonder Woman", "A comic book about Wonder Woman", 10.00, 0);
+
+insert into capstone.product (productId, productName, productDescription, price, clickCount)
+values("5828c460-419e-11ec-81d3-0242ac130003", "Ouron High School Host Club", "A manga for nerds", 10.00, 0);
